424deea4f42548398f8eab51c4484bee4e177e7a
ruska112x <56783735+ruska112x@users.noreply.github.com>
2021-11-12 20:59:32 +0600

    1

---
+++ test/fileR.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream myfile("table.txt");
+
+    std::string mytext;
+
+    while ( getline(myfile, mytext) ) {
+
+        printf("%s", mytext.c_str());
+
+    }
+
+
+    myfile.close();
+    return 0;
+}
