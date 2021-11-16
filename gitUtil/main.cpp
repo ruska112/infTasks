7773f79f5bdfcc5d489a20fc5ad64935f4068e51
ruska112 <karabalin112@gmail.com>
2021-11-16 19:00:12 +0600

    lox

---
+++ gitUtil/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    system("git add ../2itask12 ../sem ../test ../gitUtil ../LICENSE ../README.md");
+    system("git commit -m \"lox\"");
+    system("git push");
+    return 0;
+}
