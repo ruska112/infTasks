dcba53fcf7843b5af2afe3b9b572e515245aaa39
ruska112 <karabalin112@gmail.com>
2021-11-16 18:26:21 +0600

    lox

--- 2itask12/main.cpp
+++ 2itask12/main.cpp
@@ -3,6 +3,11 @@
 int const maxLines = 128;
 int const maxColumns = 128;
 
+struct Tabel {
+    int lin, col;
+    double arr[maxLines][maxColumns];
+};
+
 double Rand(int MIN, int MAX) {
     return (double) (((rand() % (10 * MAX)) + (5 * MIN))) * 0.1;
 }
@@ -26,6 +31,25 @@ void HandArrCreator(double array[][maxColumns], int lines, int columns) {
 }
 
 int MainFunc(double array[][maxColumns], int lines, int columns, double &res) {
+    /*  Функция обрабатывает массив вещественных чисел-
+     *      среди сумм элементов столбцов
+     *      находит максимальный
+     *
+     *  Входные параметры:
+     *      array - матрица
+     *          (размеры которой lines & clumns)
+     *      lines - кол-во строк
+     *      columns - кол-во столбцов
+     *
+     *  Выходной параметр:
+     *      res - максимальная сумма
+     *          элементов столбцов
+     *          (передаётся по ссылке)
+     *  
+     *  Программа возвращает:
+     *      0 - при успешной работе
+     *      1 - при возникновени ошибок
+     */
     int result = 1;
     double maxMin = -1e20;
     double tmpElement;
@@ -49,8 +73,9 @@ int MainFunc(double array[][maxColumns], int lines, int columns, double &res) {
 
 int main() {
     std::srand(time(0));
-    double arr[maxLines][maxColumns], maxColSum;
-    int lin, col, minEl, maxEl;
+    Tabel myT;
+    double maxColSum;
+    int minEl, maxEl;
     int res;
     bool mainLoop = true;
 
@@ -79,13 +104,13 @@ int main() {
             if (actArrC == 1) {
                 do {
                     printf("Enter count of lines: ");
-                    scanf("%d", &lin);
-                } while (lin <= 0 || lin > maxLines);
+                    scanf("%d", &myT.lin);
+                } while (myT.lin <= 0 || myT.lin > maxLines);
 
                 do {
                     printf("Enter count of columns: ");
-                    scanf("%d", &col);
-                } while (col <= 0 || col > maxColumns);
+                    scanf("%d", &myT.col);
+                } while (myT.col <= 0 || myT.col > maxColumns);
 
                 printf("Enter int min: ");
                 scanf("%d", &minEl);
@@ -93,28 +118,28 @@ int main() {
                 printf("Enter int max: ");
                 scanf("%d", &maxEl);
 
-                RandArrCreator(arr, lin, col, minEl, maxEl);
+                RandArrCreator(myT.arr, myT.lin, myT.col, minEl, maxEl);
 
                 myBool = 1;
             } else if (actArrC == 2) {
                 do {
                     printf("Enter count of lines: ");
-                    scanf("%d", &lin);
-                } while (lin <= 0 || lin > maxLines);
+                    scanf("%d", &myT.lin);
+                } while (myT.lin <= 0 || myT.lin > maxLines);
 
                 do {
                     printf("Enter count of columns: ");
-                    scanf("%d", &col);
-                } while (col <= 0 || col > maxColumns);
+                    scanf("%d", &myT.col);
+                } while (myT.col <= 0 || myT.col > maxColumns);
 
-                HandArrCreator(arr, lin, col);
+                HandArrCreator(myT.arr, myT.lin, myT.col);
                 myBool = 1;
             } else {
                 printf("\nERROR\n");
             }
         } else if (actFU == 2) {
             if (myBool == 1) {
-                res = MainFunc(arr, lin, col, maxColSum);
+                res = MainFunc(myT.arr, myT.lin, myT.col, maxColSum);
 
                 if (res == 0) {
                     printf("\nTable has been processed\n");
@@ -129,9 +154,9 @@ int main() {
         } else if (actFU == 3) {
             if (myBool == 1) {
                 int i, j;
-                for (i = 0; i < lin; i++) {
-                    for (j = 0; j < col; j++)
-                        printf(" %4.2lf ", arr[i][j]);
+                for (i = 0; i < myT.lin; i++) {
+                    for (j = 0; j < myT.col; j++)
+                        printf(" %4.2lf ", myT.arr[i][j]);
                     printf("\n");
                 }
             } else {
