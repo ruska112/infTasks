3f70ab0b0c7227b71b2d7c72ceec0541fab22771
ruska112 <94182636+ruska112@users.noreply.github.com>
2021-11-17 14:00:09 +0600

    Delete gitUtil directory

--- gitUtil/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    system("git add ../2itask12 ../sem ../test ../gitUtil ../LICENSE ../README.md");
-    system("git commit -m \"lox\"");
-    system("git push");
-    return 0;
-}
