8b0887def625097fb4fe2e62904d91054bb49dd3
ruska112 <karabalin112@gmail.com>
2021-11-18 12:12:16 +0600

    better code

--- useful/tableClass.cpp
+++ useful/tableClass.cpp
@@ -1,85 +1,112 @@
 #include <iostream>
 
-class Table {
-    private:
-        int rowCount;
-        int colCount;
-        double** Arr;
+class Table
+{
+private:
+    int rowCount;
+    int colCount;
+    double **Array;
 
-    public:
-        int ArrConstructor() {
-            int result = 1;
+public:
+    void ArrConstructor()
+    {
+        do
+        {
+            system("clear");
+            printf("Enter row count: ");
+            scanf("%d", &rowCount);
+        } while (rowCount < 0);
 
-            do {
-                printf("Enter row count: ");
-                scanf("%d", &rowCount);
-            } while(rowCount < 0);
-            do {
-                printf("Enter column count: ");
-                scanf("%d", &colCount);
-            } while(colCount < 0);
+        do
+        {
+            system("clear");
+            printf("Enter column count: ");
+            scanf("%d", &colCount);
+        } while (colCount < 0);
 
-            Arr = new double*[rowCount];
+        Array = new double *[rowCount];
+        for (int i = 0; i < rowCount; i++)
+            Array[i] = new double[colCount];
+
+        if (Array == NULL)
+            printf("Failed to create array!\n");
+    }
+
+    void ArrDestructure()
+    {
+        if (Array != NULL)
+        {
             for (int i = 0; i < rowCount; i++)
-                Arr[i] = new double[colCount];
-
-            if (Arr != NULL)
-                result = 0;
-
-            return result;
+                delete[] Array[i];
+            delete[] Array;
         }
+    }
 
-        void ArrDestructure() {
-           for (int i = 0; i < rowCount; i++)
-                delete[] Arr[i];
-           delete[] Arr;
-        }
+    void ArrFiller()
+    {
+        int i, j;
+        int actFU;
+        std::srand(time(0));
 
-        void ArrFiller() {
-            int i, j;
-            int actFU;
-            std::srand(time(0));
-
+        if (Array != NULL)
+        {
+            system("clear");
             printf("Choise filling method:\n");
             printf("1 - Hand\n");
             printf("2 - Rand\n");
             scanf("%d", &actFU);
-            if (actFU == 1) {
-                for (i = 0; i < rowCount; i++) {
-                    for (j = 0; j < colCount; j++) {
+            if (actFU == 1)
+            {
+                for (i = 0; i < rowCount; i++)
+                {
+                    for (j = 0; j < colCount; j++)
+                    {
+                        system("clear");
                         printf("[%d][%d] = ", i, j);
-                        scanf("%lf", &Arr[i][j]);
+                        scanf("%lf", &Array[i][j]);
                     }
                 }
-            } else if (actFU == 2) {
+            }
+            else if (actFU == 2)
+            {
                 int MIN, MAX;
                 printf("Enter min value: ");
                 scanf("%d", &MIN);
                 printf("Enter max value: ");
                 scanf("%d", &MAX);
 
-                for (i = 0; i < rowCount; i++) {
-                    for (j = 0; j < colCount; j++) {
-                        Arr[i][j] = (double) (((rand() % (10 * MAX)) + (5 * MIN))) * 0.1 ;
+                for (i = 0; i < rowCount; i++)
+                {
+                    for (j = 0; j < colCount; j++)
+                    {
+                        Array[i][j] = (double)(((rand() % (100 * MAX)) + (50 * MIN))) * 0.01;
                     }
                 }
-
             }
-
         }
+        else
+        {
+            printf("\nError!\n");
+        }
+    }
 
-        void ArrPrintf() {
-            int i, j;
-
-            for (i = 0; i < rowCount; i++) {
+    void ArrPrintf()
+    {
+        int i, j;
+        if (Array != NULL)
+        {
+            for (i = 0; i < rowCount; i++)
+            {
                 for (j = 0; j < colCount; j++)
-                    printf("%3.1lf ", Arr[i][j]);
+                    printf("%4.2lf ", Array[i][j]);
                 printf("\n");
             }
         }
+    }
 };
 
-int main() {
+int main()
+{
 
     Table myArr;
 
