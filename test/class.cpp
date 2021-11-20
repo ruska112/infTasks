b1f65c1e9fa362777b150d1da6f90831c40aa9c2
ruska112 <karabalin112@gmail.com>
2021-11-20 16:02:31 +0600

    test:]

--- test/class.cpp
+++ test/class.cpp
@@ -1,59 +1,70 @@
 #include <iostream>
 
-class Table {
-    private:
-        int rowCount;
-        int colCount;
-        double** Arr;
+class Table
+{
+private:
+    int rowCount;
+    int colCount;
+    double **Arr;
 
-    public:
-        int ArrCreator() {
-            int result = 1;
+public:
+    int ArrCreator()
+    {
+        int result = 1;
 
-            do {
-                printf("Enter row count: ");
-                scanf("%d", &rowCount);
-            } while(rowCount < 0);
-            do {
-                printf("Enter column count: ");
-                scanf("%d", &colCount);
-            } while(colCount < 0);
+        do
+        {
+            printf("Enter row count: ");
+            scanf("%d", &rowCount);
+        } while (rowCount < 0);
+        do
+        {
+            printf("Enter column count: ");
+            scanf("%d", &colCount);
+        } while (colCount < 0);
 
-            if (rowCount > 0 && colCount > 0) {
-                Arr = new double*[rowCount];
-                for (int i = 0; i < rowCount; i++)
-                    Arr[i] = new double[colCount];
-                int i, j;
-                for (i = 0; i < rowCount; i++) {
-                    for (j = 0; j < colCount; j++) {
-                        printf("[%d][%d] = ", i, j);
-                        scanf("%lf", &Arr[i][j]);
-                    }
-                }
-                if (Arr != NULL)
-                    result = 0;
-            }
-            return result;
-        }
-
-        void ArrDestructure() {
-           for (int i = 0; i < rowCount; i++)
-                delete[] Arr[i];
-           delete[] Arr;
-        }
-        
-        void ArrPrintf() {
+        if (rowCount > 0 && colCount > 0)
+        {
+            Arr = new double *[rowCount];
+            for (int i = 0; i < rowCount; i++)
+                Arr[i] = new double[colCount];
             int i, j;
-
-            for (i = 0; i < rowCount; i++) {
+            for (i = 0; i < rowCount; i++)
+            {
                 for (j = 0; j < colCount; j++)
-                    printf("%3.1lf ", Arr[i][j]);
-                printf("\n");
+                {
+                    printf("[%d][%d] = ", i, j);
+                    scanf("%lf", &Arr[i][j]);
+                }
             }
+            if (Arr != NULL)
+                result = 0;
         }
+        return result;
+    }
+
+    void ArrDestructure()
+    {
+        for (int i = 0; i < rowCount; i++)
+            delete[] Arr[i];
+        delete[] Arr;
+    }
+
+    void ArrPrintf()
+    {
+        int i, j;
+
+        for (i = 0; i < rowCount; i++)
+        {
+            for (j = 0; j < colCount; j++)
+                printf("%3.1lf ", Arr[i][j]);
+            printf("\n");
+        }
+    }
 };
 
-int main() {
+int main()
+{
 
     Table myArr;
 
